f3f50cf2dfa6a1e82e4ed95bcd33b58cf4ce76de
Christian Michael Paul Bankester <cbankester@immense.net>
2015-12-24 17:11:29 -0600

    Added koa; added better connection handling

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-"use strict";
-const app = require('http').createServer(handler);
-const WebSocketServer = require('./lib/api/websocket-server');
-
-const webSocketServer = new WebSocketServer(app);
-
-function handler(req, res) {
-  res.writeHead(200);
-  res.end();
-}
-
-app.listen(1337, () => {
-  console.log(`${new Date()} Server is listening on port 1337`);
-});
+require('./lib/server.js');
